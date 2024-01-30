d9f454827ba22b5d9f7a7fb9975cb6b0bb2d22bd
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-01-30 22:50:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const logo = document.getElementById('dusk-logo');
     let x = 0, y = 0;
     let xSpeed = 2, ySpeed = 2;
-    const logoWidth = 150; // Adjust to match the actual width of your logo
-    const logoHeight = 150; // Adjust to match the actual height of your logo
+    const logoWidth = logo.offsetWidth;
+    const logoHeight = logo.offsetHeight;
 
     function bounceLogo() {
-        x += xSpeed;
-        y += ySpeed;
-
+        // Invert speed when logo hits the edge of the window
         if (x + logoWidth > window.innerWidth || x < 0) {
             xSpeed *= -1;
         }
-
         if (y + logoHeight > window.innerHeight || y < 0) {
             ySpeed *= -1;
         }
 
+        // Update logo position
+        x += xSpeed;
+        y += ySpeed;
         logo.style.left = x + 'px';
         logo.style.top = y + 'px';
 
         requestAnimationFrame(bounceLogo);
     }
+
+    // Center logo starting position
+    x = (window.innerWidth - logoWidth) / 2;
+    y = (window.innerHeight - logoHeight) / 2;
 
     bounceLogo();
 });
